f0e57ed5dec7ec080953c8e7c3ad211c21b3c36c
thexovc <osazeepeter79@gmail.com>
2022-12-11 15:49:45 +0100

    added create comment

--- contracts/Kobis.sol
+++ contracts/Kobis.sol
@@ -47,10 +47,10 @@ contract Kosbis {
         string post;
         uint256 time;
         bool hideIdentity;
-        address addr;
+        address author;
         uint256 community;
         fileContent file;
-        mapping(address => CommentContent) comment;
+        uint256 [] comments;
     }
 
     struct fileContent {
@@ -59,11 +59,13 @@ contract Kosbis {
     }
 
     struct CommentContent {
-        address payable addr;
+        address payable author;
+        uint256 comId;
+        uint256 postId;
         string post;
         uint256 time;
-        fileContent [] file;
-        mapping(address => NestedCommentContent) comment;
+        fileContent file;
+        // CommentContent[] comments;
     }
 
     struct NestedCommentContent {
@@ -75,7 +77,8 @@ contract Kosbis {
 
     mapping(uint256 => CommunityContent) public communityProfile;
     mapping(address => Profile) public User;
-    mapping(address => PostContent []) public Post;
+    mapping(uint256 => PostContent []) public Post;
+    mapping(address => CommentContent []) public Comment;
 
     function createUser(
         string memory name,
@@ -110,22 +113,50 @@ contract Kosbis {
         string memory fileIpfs,
         string memory fileType
     )  public  {
-        uint256 myPost = User[msg.sender].postNum;
-
+      
         fileContent memory newFile;
         newFile.fileIpfs = fileIpfs;
         newFile.fileType = fileType;
 
-        Post[msg.sender][myPost].addr = msg.sender;
-        Post[msg.sender][myPost].name = name;
-        Post[msg.sender][myPost].post = post;
-        Post[msg.sender][myPost].time = block.timestamp;
-        Post[msg.sender][myPost].hideIdentity = hideIdentity;
-        Post[msg.sender][myPost].community = community;
-        Post[msg.sender][myPost].file = newFile;
+        PostContent memory newPost;
+
+        newPost.author = msg.sender;
+        newPost.name = name;
+        newPost.post = post;
+        newPost.time = block.timestamp;
+        newPost.hideIdentity = hideIdentity;
+        newPost.community = community;
+        newPost.file = newFile;
+
+        Post[community].push(newPost);
 
         communityProfile[community].postNum++;  
         User[msg.sender].postNum++; 
+    }
+
+    function createComment(
+        uint256 comId,
+        uint256 postId,
+        string memory text,
+        string memory fileIpfs,
+        string memory fileType
+    )public {
+        fileContent memory newFile;
+        newFile.fileIpfs = fileIpfs;
+        newFile.fileType = fileType;
+
+        CommentContent memory newComment;
+
+        newComment.author = payable(msg.sender);
+        newComment.comId = comId;
+        newComment.postId = postId; 
+        newComment.post = text; 
+        newComment.time = block.timestamp;
+        newComment.file = newFile;
+
+        Comment[msg.sender].push(newComment);
+
+
     }
 
 }
